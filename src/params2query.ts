0b12c33c251b6c131f7f6c81317bd7b8bf1dda05
Trần Phương Nam <tranphuongnam@jv-it.com.vn>
2023-09-28 15:02:51 +0700

    - Add falsy test case for params2query

--- src/params2query.ts
+++ src/params2query.ts
@@ -1,4 +1,4 @@
-export type QueryParamsType = Record<string | number, any>;
+export type QueryParamsType = Record<string | number, any> | null;
 
 function encodeQueryParam(key: string, value: any) {
   const encodedKey = encodeURIComponent(key);
@@ -8,15 +8,17 @@ function encodeQueryParam(key: string, value: any) {
 }
 
 function addQueryParam(query: QueryParamsType, key: string) {
-  return encodeQueryParam(key, query[key]);
+  return encodeQueryParam(key, query?.[key]);
 }
 
 function addArrayQueryParam(query: QueryParamsType, key: string) {
-  const value = query[key];
+  const value = query?.[key];
   return value.map((v: any) => encodeQueryParam(key, v)).join("&");
 }
 
 export function toQueryString(query: QueryParamsType = {}): string {
+  if (query === null) return "";
+
   const getQueryItem = (key: string) => {
     if (Array.isArray(query[key])) return addArrayQueryParam(query, key);
     return addQueryParam(query, key);
--- tests/params2query.test.ts
+++ tests/params2query.test.ts
@@ -1,17 +1,29 @@
-import { expect, test } from "vitest";
+import { expect, test, describe } from "vitest";
 import { toQueryString } from "../src/params2query";
 
-test("params2query", () => {
-  const params = {
-    id: 1,
-    offset: 10,
-    page: 2,
-    filter: ["increase", "counted"],
-    category_id: "adidas",
-  };
-  const queryString = toQueryString(params);
+describe("params2query", () => {
+  test("Convert empty or invalid params", () => {
+    const emptyParam = {};
+    const invalidParams = [undefined, true, false, null];
 
-  const result =
-    "id=1&offset=10&page=2&filter=increase&filter=counted&category_id=adidas";
-  expect(queryString).toBe(result);
+    const params = [emptyParam, ...invalidParams];
+    params.forEach((p) => {
+      const queryString = toQueryString(p);
+      expect(queryString).toBe("");
+    });
+  });
+  test("Convert valid params", () => {
+    const params = {
+      id: 1,
+      offset: 10,
+      page: 2,
+      filter: ["increase", "counted"],
+      category_id: "adidas",
+    };
+    const queryString = toQueryString(params);
+
+    const result =
+      "id=1&offset=10&page=2&filter=increase&filter=counted&category_id=adidas";
+    expect(queryString).toBe(result);
+  });
 });
